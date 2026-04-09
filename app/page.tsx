b9e11a8e83b7c04719ba4196404024faee037010
ng <nicolai@ngb.studio>
2026-04-08 23:14:24 -0400

    Simplified

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,8 @@ import { SignupForm } from "./signup-form";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-background font-sans">
-      <main className="flex flex-1 w-full max-w-lg flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <Card className="w-full">
-          <CardTitle className="px-6">Sign up for the latest events</CardTitle>
-          <CardContent>
-            <SignupForm />
-          </CardContent>
-        </Card>
+      <main className="flex flex-1 w-full max-w-md flex-col items-center justify-center px-4 sm:items-start">
+        <SignupForm />
       </main>
     </div>
   );
--- app/signup-form.tsx
+++ app/signup-form.tsx
@@ -1,18 +1,17 @@
 "use client";
 
-import * as React from "react";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, Form, useForm } from "react-hook-form";
-import { Input } from "@/components/ui/input";
-import {
-  Field,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/ui/field";
-import { Button } from "@/components/ui/button";
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
 import { toast } from "sonner";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { ArrowBigUpIcon, CornerDownLeftIcon } from "lucide-react";
 
 const schema = z.object({
   email: z.email({ message: "Not so fast ;)" }),
@@ -21,7 +20,7 @@ const schema = z.object({
 export function SignupForm() {
   const form = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
-    mode: "onBlur",
+    mode: "onTouched",
     defaultValues: {
       email: "",
     },
@@ -32,32 +31,39 @@ export function SignupForm() {
   };
 
   return (
-    <form onSubmit={form.handleSubmit(onSubmit)}>
-      <FieldGroup>
-        <Controller
-          name="email"
-          control={form.control}
-          render={({ field, fieldState }) => (
-            <Field data-invalid={fieldState.invalid}>
-              <FieldLabel htmlFor="email">Email</FieldLabel>
-              <Input
+    <form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+      <Controller
+        name="email"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid} className="relative">
+            <FieldLabel htmlFor="email">Tap in</FieldLabel>
+            <InputGroup>
+              <InputGroupInput
                 id="email"
                 type="email"
+                placeholder="Email"
+                required
                 aria-invalid={fieldState.invalid}
+                aria-describedby={fieldState.error ? "email-error" : undefined}
                 {...field}
               />
-              {fieldState.error && (
-                <FieldError>{fieldState.error?.message}</FieldError>
-              )}
-            </Field>
-          )}
-        />
-        <Field>
-          <Button type="submit" disabled={!form.formState.isDirty}>
-            Submit
-          </Button>
-        </Field>
-      </FieldGroup>
+              <InputGroupAddon align="inline-end">
+                <InputGroupButton
+                  type="submit"
+                  size="icon-xs"
+                  disabled={fieldState.invalid}
+                >
+                  <ArrowBigUpIcon />
+                </InputGroupButton>
+              </InputGroupAddon>
+            </InputGroup>
+            <FieldDescription>
+              Receive updates on our latest events. No spam. All love.
+            </FieldDescription>
+          </Field>
+        )}
+      />
     </form>
   );
 }
